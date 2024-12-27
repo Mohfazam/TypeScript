fdc6d3f1d6b051f25e0331710dd8f62392207662
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-27 15:06:37 +0530

    Fun2

--- Fun2/index.ts
+++ Fun2/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+function sum1(a: number, b: number){
+    return a + b;
+}
+
+function sum2(c: number, d:number){
+    return c + d;
+}
+
+
+
+function finalSum(x: number, y: number){
+    return x+y;
+}
+
+
+
+const res1 = sum1(2,2);//4
+const res2 = sum2(4,4);//8
+
+
+const final = finalSum(res1, res2);//8+4 == 12
+
+console.log(final);
