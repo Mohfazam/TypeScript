6ccac3decfaf488b695bc5956055a4fe594f32ee
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-27 15:19:26 +0530

    Fun3

--- Fun1/index.ts
+++ Fun1/index.ts
@@ -1,5 +1,5 @@
 import promptsync from "prompt-sync";
-
+// npm install --save-dev @types/prompt-sync          == need to run this command before using prompt-sunc
 const prompt = promptsync();
 
 function name() {
--- Fun3/index.ts
+++ Fun3/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello ");
+
+import promptsync from "prompt-sync";
+// npm install --save-dev @types/prompt-sync          == need to run this command before using prompt-sunc
+const prompt = promptsync();
+
+function isLegal(age: number){
+    if(age >= 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+const ans = isLegal(Number(prompt("Enter your age: ")));
+if(ans){
+    console.log("Can Drive");
+}
+else{
+    console.log("Can't drive");
+}
