2d7779c8045b0b4bb230738414a6ca318cafe77c
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-27 15:29:05 +0530

    Fun3

--- Fun2/index.ts
+++ Fun2/index.ts
@@ -1,23 +1,17 @@
-function sum1(a: number, b: number){
+function sum1(a: number, b: number): number{
     return a + b;
 }
 
-function sum2(c: number, d:number){
+function sum2(c: number, d:number): number{
     return c + d;
 }
 
-
-
-function finalSum(x: number, y: number){
+function finalSum(x: number, y: number): number{
     return x+y;
 }
 
-
-
 const res1 = sum1(2,2);//4
 const res2 = sum2(4,4);//8
-
-
 const final = finalSum(res1, res2);//8+4 == 12
 
 console.log(final);
