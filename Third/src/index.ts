f4fd00e205febd8881da7fdf3757368c6b810937
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-27 16:55:59 +0530

    Third

--- Third/src/index.ts
+++ Third/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+function greet(user: {
+    name: string;
+    age: number;
+}) {
+    console.log(`name: ${user.name} and age is : ${user.age}`);
+}
+
+
+const  user: { name: string, age: number } = {name : "Sarwar", age: 25};
+
+greet(user);
