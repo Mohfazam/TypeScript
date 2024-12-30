6ed7a7de1a25a016e5824e16df17b3d5eebfc412
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-30 14:42:18 +0530

    Types

--- Types/src/index.ts
+++ Types/src/index.ts
@@ -1,1 +1,19 @@
 console.log("Hello");
+
+type employee = {
+    name: string;
+    age: number;
+}
+
+type manager = {
+    name: string;
+    age: number;
+}
+
+
+type teamlead = employee & manager;
+
+let t: teamlead = {
+    name: "Sarwar",
+    age: 21
+}
