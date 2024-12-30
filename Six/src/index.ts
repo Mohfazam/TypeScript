9156ac59ef123d12e198ca64c9eb97bebeb5dae8
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-30 15:53:22 +0530

    Sixth Solved

--- Six/src/index.ts
+++ Six/src/index.ts
@@ -1,7 +1,23 @@
 // Given an list of Users, filter out the users that are legal to setDriver(age > 19)
 console.log("Hello");
 
-
-function candrive(age: number){
-
+interface User{
+    name: string;
+    lastname: string;
+    age: number;
 }
+
+
+function candrive(value: User):boolean{
+    return value.age> 18;
+}
+
+
+let  user1: User = {
+    name: "Sarwar",
+    lastname: "Khan",
+    age: 1
+}
+
+let ans = candrive(user1);
+console.log(ans);
