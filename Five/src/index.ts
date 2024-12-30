2027388421d2f41f7e48b74cbaef56d6d216df53
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-30 15:35:47 +0530

    Fifth

---
+++ Five/src/index.ts
@@ -0,0 +1,17 @@
+//Given an array of positive integers as input, return the maximum value in the array
+
+function maximum(num: number[]): number{
+    let max = Infinity;
+
+    for(let i = 0; i < num.length; i++){
+        if(num[i] > max){
+            max = num[i];
+        }
+    }
+    return max;
+}
+
+let x = [2,4, 6, 82, 346, 347, 235, 387, 90000];
+
+let ans = maximum(x);
+console.log(ans);
