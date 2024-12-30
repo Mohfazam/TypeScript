e1ac0b5086804a1438693c961e6cdda44bf050c3
Mohfazam <mohd.sarwar.code@gmail.com>
2024-12-30 15:21:48 +0530

    Fourth

--- Fourth/src/index.ts
+++ Fourth/src/index.ts
@@ -1,1 +1,26 @@
 console.log("hello");
+
+type Admin = {
+    name: string;
+    role: string;
+}
+
+type User =  Admin;
+
+let user1: User = {
+    name: "sarwar",
+    role: "User"
+} 
+
+let user2: User = {
+    name: "Mohfazam",
+    role: "Admin"
+} 
+
+function greet(value :Admin | User): string{
+    return `Welcome ${value.name} and role is ${value.role}`;
+}
+
+let result = greet(user1);
+console.log(result);
+console.log(greet(user2));
