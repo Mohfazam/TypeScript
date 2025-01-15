6746226d762d4b20d47ddf2f378c1d9bd6fa3ede
Mohfazam <mohd.sarwar.code@gmail.com>
2025-01-15 17:42:23 +0530

    Eight 3

--- Eight/src/index.ts
+++ Eight/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello nigga");
+interface user{
+    name: string;
+    age: number;
+}
+
+const user1: user = {
+    name: "Sarwar",
+    age: 10
+}
+
+const user2: user = {
+    name: "mohfazam",
+    age: 20
+}
+
+function sumOfAge(x:user, y:user):number{
+    return x.age + y.age;
+}
+
+console.log(sumOfAge(user1, user2));
